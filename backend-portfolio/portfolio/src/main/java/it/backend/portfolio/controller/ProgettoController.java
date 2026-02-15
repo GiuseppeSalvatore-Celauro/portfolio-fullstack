c1b2e4191bc84bb1057e3c61bd454523a175b7b5
GiuseppeSalvatore-Celauro <c.peppe.salvo@gmail.com>
2026-02-15 21:52:42 +0100

    Aggiunta la funzione put

--- backend-portfolio/portfolio/src/main/java/it/backend/portfolio/controller/ProgettoController.java
+++ backend-portfolio/portfolio/src/main/java/it/backend/portfolio/controller/ProgettoController.java
@@ -1,5 +1,7 @@
 package it.backend.portfolio.controller;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import it.backend.portfolio.dto.ProgettoDTO;
 import it.backend.portfolio.model.Img;
@@ -73,20 +77,57 @@ public class ProgettoController {
 	
 	@GetMapping("/progetti/{id}")
 	public ResponseEntity<ProgettoResponse> vediProgetto(@PathVariable Long id){
-		if(!progettiRepository.existsById(id)) {
-			return ResponseEntity.notFound().build();
-		}
-		
-		Progetto progetto = progettiRepository.findById(id).get();
+		Progetto progetto = progettiRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
 		return ResponseEntity.ok(new ProgettoResponse(progetto));
 	}
 	
 	@DeleteMapping("/progetti/{id}")
 	public ResponseEntity<Void> eliminaProgetto(@PathVariable Long id){
-		if(!progettiRepository.existsById(id)) {
-			return ResponseEntity.notFound().build();
+		Progetto progetto = progettiRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		progettiRepository.delete(progetto);
+		return ResponseEntity.ok().build();
+	}
+	
+	@PutMapping("/progetti/{id}")
+	public ResponseEntity<ProgettoResponse> modificaProgetto(@PathVariable Long id, @RequestBody ProgettoDTO body){
+		
+		Progetto progetto = progettiRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		
+		progetto.setDescrizione(body.getDescrizione() != null ? body.getDescrizione() : progetto.getDescrizione());
+		progetto.setNome(body.getNome() != null ? body.getNome() : progetto.getNome());
+		progetto.setUrl(body.getUrl() != null ? body.getUrl() : progetto.getUrl());
+		
+		if(body.getImmagini() != null) {
+			
+			for(Img img: new ArrayList<>(progetto.getImmagini())) {
+				progetto.removeImg(img);
+			}
+			
+			for(String urlImg: body.getImmagini()) {
+				Img img = new Img(urlImg);
+				progetto.addImg(img);
+			}
+			
 		}
-		progettiRepository.deleteById(id);
-		return ResponseEntity.noContent().build();
+		
+		if(body.getTecnologie() != null) {
+			
+			for(Tecnologia tech: new HashSet<>(progetto.getTecnologie())) {
+				progetto.removeTecnologia(tech);
+			}
+			
+			for(String techName: body.getTecnologie()) {
+				Tecnologia tecnologia = techRepository.findByNome(techName);
+				
+				if(tecnologia == null) {
+					tecnologia = techRepository.save(new Tecnologia(techName));
+				}
+				
+				progetto.addTecnologia(tecnologia);
+			}
+		}
+
+		Progetto saved = progettiRepository.save(progetto);
+		return ResponseEntity.ok(new ProgettoResponse(saved));
 	}
 }
